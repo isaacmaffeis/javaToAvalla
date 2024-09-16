5c5bc0e344bebf4270bc3cefa5585462f595f185
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-16 12:14:40 +0200

    added javadoc comments

---
+++ src/main/java/org/javaToAvalla/avallaScenario/impl/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This package contains implementations of the interfaces defined in the
+ * {@code avallaScenario} package.
+ *
+ * <p>The {@code ScenarioListMapper} class implements the
+ * {@link org.javaToAvalla.avallaScenario.ScenarioListMapperIF} interface, providing
+ * functionality to convert a list of {@link org.javaToAvalla.model.Scenario}
+ * objects into a list of {@link org.javaToAvalla.model.ScenarioFile} objects.</p>
+ *
+ * <p>The {@code ScenarioWriter} class implements the
+ * {@link org.javaToAvalla.avallaScenario.ScenarioWriterIF} interface, providing the
+ * ability to write a {@link org.javaToAvalla.model.Scenario} into a
+ * {@link org.javaToAvalla.model.ScenarioFile}.</p>
+ */
+package org.javaToAvalla.avallaScenario.impl;
---
+++ src/main/java/org/javaToAvalla/fileWriter/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the interface and implementation for writing
+ * {@link org.javaToAvalla.model.ScenarioFile} objects to a file.
+ */
+
+package org.javaToAvalla.fileWriter;

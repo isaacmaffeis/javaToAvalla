f0d51327f1f6d66d02f25b47aa6b1ea9cc673e1d
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-19 11:51:27 +0200

    Create package-info.java

---
+++ src/main/java/com/javatoavalla/stepfunction/impl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the implementation classes of the interfaces in the
+ * {@link com.javatoavalla.stepfunction} package.
+ */
+package com.javatoavalla.stepfunction.impl;
